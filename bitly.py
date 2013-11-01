0fca61ef34528fc6d32e1e38c65cd42a1b24983e
Amy Roberts <uncamy@gmail.com>
2013-11-01 14:45:18 -0400

    added form request

--- bitly.py
+++ bitly.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def bite():
+def bitly():
     return 'starting my bit.ly clone'
 
 if __name__ == '__main__':
-    app.run()
+#make externally visable-- Turn off degugger!
+    #app.run(host='0.0.0.0')
+#development mode
+    app.run(debug= True)
+
+@app.route('/getaddress', methods = ['POST', 'GET'])
+def getaddress():
+    error = None
+    if request.method == 'POST':
+        if valid_address(request.form['web_address']):
+            return log_address(request.form['web_address'])
+        else:
+            error = 'Valid web adress'
+    return render_template('getaddress.html', error=error)
