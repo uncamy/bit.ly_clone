558534513ebd0d7854cee7cf7d56cb54b341216a
Amy Roberts <uncamy@gmail.com>
2013-11-01 14:50:47 -0400

    added empty test case

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import tempfile
 
 class BitlyTestCase(unittest.TestCase):
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.db_fd, bitly.app.config['DATABASE'] = tempfile.mkstemp()
         bitly.app.config['TESTING'] = True
         self.app = bitly.app.test_client()
         bitly.init_db()
@@ -13,6 +13,10 @@ class BitlyTestCase(unittest.TestCase):
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(bitly.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No urls entered' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
